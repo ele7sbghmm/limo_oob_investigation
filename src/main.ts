1db69445020ebdd2f0814d6fab5b12fbf7a1dd8c
ims <elementium7@gmail.com>
2025-10-01 00:07:42 +1300

    import.meta.env.BASE_URL

--- src/main.ts
+++ src/main.ts
@@ -8,61 +8,62 @@ type Fence = { start: Vec3, end: Vec3, normal: Vec3 }
 let dummy = new THREE.Object3D()
 let instancedCircles: THREE.InstancedMesh
 
-const fences = await fetch('./data/fences.json').then(data => data.json())
+const url = import.meta.env.BASE_URL + './data/fences.json'
+const fences = await fetch(url).then(data => data.json())
 const halfLengthSqrs: number[] = Array.from({ length: fences.length }, () => 0.)
 
 const fencesToCircle = (fences: Fence[]) => {
-	const circleGeometry = new THREE.CircleGeometry(1., 96., 0., Math.PI * 2.)
-	const material = new THREE.MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: .1, depthWrite: false, side: THREE.DoubleSide })
-	instancedCircles = new THREE.InstancedMesh(circleGeometry, material, fences.length)
-	scene.add(instancedCircles)
+  const circleGeometry = new THREE.CircleGeometry(1., 96., 0., Math.PI * 2.)
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: .1, depthWrite: false, side: THREE.DoubleSide })
+  instancedCircles = new THREE.InstancedMesh(circleGeometry, material, fences.length)
+  scene.add(instancedCircles)
 
-	fences.forEach((fence, i) => {
-		const [sx, sz, ex, ez] = [fence.start.x, -fence.start.z, fence.end.x, -fence.end.z]
-		const center = [(sx + ex) / 2., (sz + ez) / 2.]
+  fences.forEach((fence, i) => {
+    const [sx, sz, ex, ez] = [fence.start.x, -fence.start.z, fence.end.x, -fence.end.z]
+    const center = [(sx + ex) / 2., (sz + ez) / 2.]
 
-		const [xdif, zdif] = [Math.abs(center[0] - sx), Math.abs(center[1] - sz)]
-		halfLengthSqrs[i] = xdif * xdif + zdif * zdif
+    const [xdif, zdif] = [Math.abs(center[0] - sx), Math.abs(center[1] - sz)]
+    halfLengthSqrs[i] = xdif * xdif + zdif * zdif
 
-		dummy.position.set(center[0], 0., center[1])
-		dummy.rotation.x = Math.PI / 2.
-		dummy.updateMatrix()
+    dummy.position.set(center[0], 0., center[1])
+    dummy.rotation.x = Math.PI / 2.
+    dummy.updateMatrix()
 
-		instancedCircles.setMatrixAt(i, dummy.matrix)
-	})
+    instancedCircles.setMatrixAt(i, dummy.matrix)
+  })
 
-	instancedCircles.instanceMatrix.needsUpdate = true
+  instancedCircles.instanceMatrix.needsUpdate = true
 
-	return instancedCircles
+  return instancedCircles
 }
 
 const updateInstanceScales = (scale: number) => {
-	for (let i = 0; i < instancedCircles.count; i++) {
-		instancedCircles.getMatrixAt(i, dummy.matrix)
+  for (let i = 0; i < instancedCircles.count; i++) {
+    instancedCircles.getMatrixAt(i, dummy.matrix)
 
-		dummy.matrix.decompose(dummy.position, dummy.quaternion, dummy.scale)
+    dummy.matrix.decompose(dummy.position, dummy.quaternion, dummy.scale)
 
-		const radius = Math.sqrt(halfLengthSqrs[i] + scale * scale)
-		dummy.scale.set(radius, radius, radius)
+    const radius = Math.sqrt(halfLengthSqrs[i] + scale * scale)
+    dummy.scale.set(radius, radius, radius)
 
-		dummy.updateMatrix()
-		instancedCircles.setMatrixAt(i, dummy.matrix)
-	}
+    dummy.updateMatrix()
+    instancedCircles.setMatrixAt(i, dummy.matrix)
+  }
 
-	instancedCircles.instanceMatrix.needsUpdate = true
+  instancedCircles.instanceMatrix.needsUpdate = true
 }
 
 const fencesToMesh = (scene: THREE.Scene, fences: Fence[]) => {
-	const points = fences.flatMap(fence => [fence.start.x, 0., -fence.start.z, fence.end.x, 0., -fence.end.z])
-	const arrayBuf = new Float32Array(points)
+  const points = fences.flatMap(fence => [fence.start.x, 0., -fence.start.z, fence.end.x, 0., -fence.end.z])
+  const arrayBuf = new Float32Array(points)
 
-	const geometry = new THREE.BufferGeometry()
-	geometry.setAttribute('position', new THREE.BufferAttribute(arrayBuf, 3))
+  const geometry = new THREE.BufferGeometry()
+  geometry.setAttribute('position', new THREE.BufferAttribute(arrayBuf, 3))
 
-	const material = new THREE.LineBasicMaterial({ color: 0xff0000 })
+  const material = new THREE.LineBasicMaterial({ color: 0xff0000 })
 
-	const lines = new THREE.LineSegments(geometry, material)
-	scene.add(lines)
+  const lines = new THREE.LineSegments(geometry, material)
+  scene.add(lines)
 }
 
 
@@ -77,9 +78,9 @@ const camera = new THREE.PerspectiveCamera(75., window.innerWidth / window.inner
 camera.position.z = 5.
 
 window.addEventListener('resize', () => {
-	camera.aspect = window.innerWidth / window.innerHeight
-	camera.updateProjectionMatrix()
-	renderer.setSize(window.innerWidth, window.innerHeight)
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
 const geometry = new THREE.BoxGeometry(1., 1., 1.)
@@ -96,25 +97,25 @@ camera.position.set(controls.target.x, 100., controls.target.z)
 controls.enableRotate = false
 
 const guiControls = {
-	wheelBase: 11.66
+  wheelBase: 11.66
 }
 
 const gui = new GUI()
 gui
-	.add(guiControls, 'wheelBase', 5., 11.66)
-	.name('wheel base')
-	.step(.01)
-	.onChange(updateInstanceScales)
+  .add(guiControls, 'wheelBase', 5., 11.66)
+  .name('wheel base')
+  .step(.01)
+  .onChange(updateInstanceScales)
 
 const animate = () => {
-	requestAnimationFrame(animate)
+  requestAnimationFrame(animate)
 
-	mesh.rotation.x += .01
-	mesh.rotation.z += .01
+  mesh.rotation.x += .01
+  mesh.rotation.z += .01
 
-	controls.update(.1)
+  controls.update(.1)
 
-	renderer.render(scene, camera)
+  renderer.render(scene, camera)
 }
 
 animate()
