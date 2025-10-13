3f21f7e06a607ef4d62474b6b3e8e62ddf77a97b
ims <elementium7@gmail.com>
2025-10-14 12:42:13 +1300

    idk

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import paper from 'paper'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+paper.setup('canvas')
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const circle = new paper.Path.Circle({
+  center: paper.view.center,
+  radius: 200,
+  fillColor: 'red'
+})
+
+paper.view.onFrame = event => {
+  circle.rotate(1. + event.count * 0)
+  circle.scaling = 1. + .05 * Math.sin(event.time * 2.)
+}
+
